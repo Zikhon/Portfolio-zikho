ace3be19b319aba4030e2a7000b96aeab25156e1
Zikhona Mdingazwe <zikhonamdingazwe@gmail.com>
2023-08-29 13:49:44 +0200

    small change in validations

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -23,8 +23,62 @@ function opentab(tabname) {
   document.getElementById(tabname).classList.add("active-tab");
 }
 
-// form vaidation
+// contact-right vaidation
+// Set Success Message
+function setError(input, message) {
+  const contactForm = input.parentElement;
+  contactForm.classList.remove("success"); // Remove success class
+  contactForm.classList.add("error"); // Add error class
+  const errorText = contactForm.querySelector(".error-text");
+  errorText.innerText = message;
+}
 
+function setSuccess(input) {
+  const contactForm = input.parentElement;
+  contactForm.classList.remove("error"); // Remove error class
+  contactForm.classList.add("success"); // Add success class
+  const errorText = contactForm.querySelector(".error-text");
+  errorText.innerText = ""; // Clear error text
+}
+
+function validEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validateForm() {
+  const contactForm = document.getElementById("contactForm");
+
+  const nameInput = contactForm.elements.name;
+  const emailInput = contactForm.elements.email;
+  const messageInput = contactForm.elements.message;
+
+  if (nameInput.value.trim() === "") {
+    setError(nameInput, "Name cannot be blank");
+    return false;
+  } else {
+    setSuccess(nameInput);
+  }
+
+  if (emailInput.value.trim() === "") {
+    setError(emailInput, "Email cannot be blank");
+    return false;
+  } else if (!validEmail(emailInput.value.trim())) {
+    setError(emailInput, "Email is not valid");
+    return false;
+  } else {
+    setSuccess(emailInput);
+  }
+
+  if (messageInput.value.trim() === "") {
+    setError(messageInput, "Message cannot be blank");
+    return false;
+  } else {
+    setSuccess(messageInput);
+  }
+
+  return true;
+}
 
 
 
